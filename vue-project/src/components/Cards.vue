e6da4224139c0a94e70bdb61c780d1ba22298d01
Kev from Work <kevingullytps@gmail.com>
2024-08-08 20:05:00 -0300

    Apply vue.js to project

---
+++ vue-project/src/components/Cards.vue
@@ -0,0 +1,75 @@
+<template>
+    <section>
+        <h1 class="display-3">Principais Frameworks Front-End</h1>
+    </section>
+
+    <section>
+        <div class="framework">
+            <h4 class="display-6">MaterializeCSS</h4>
+            <button class="btn btn-primary">Saiba mais</button>
+        </div>
+        <div class="framework">
+            <h4 class="display-6">Bulma</h4>
+            <button class="btn btn-secondary">Saiba mais</button>
+        </div>
+        <div class="framework">
+            <h4 class="display-6">Tailwind CSS</h4>
+            <button class="btn btn-success">Saiba mais</button>
+        </div>
+        <div class="framework">
+            <h4 class="display-6">Bootstrap</h4>
+            <button class="btn btn-danger">Saiba mais</button>
+        </div>
+        <div class="framework">
+            <h4 class="display-6">Angular</h4>
+            <button class="btn btn-warning">Saiba mais</button>
+        </div>
+        <div class="framework">
+            <h4 class="display-6">Vue.js</h4>
+            <button class="btn btn-info">Saiba mais</button>
+        </div>
+        <div class="framework">
+            <h4 class="display-6">Ember</h4>
+            <button class="btn btn-light">Saiba mais</button>
+        </div>
+        <div class="framework">
+            <h4 class="display-6">Svelte</h4>
+            <button class="btn btn-dark">Saiba mais</button>
+        </div>
+    </section>
+</template>
+
+
+<style scoped>
+main section {
+    padding: 30px 30px 15px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+
+h1 {
+    align-self: center;
+}
+
+.framework {
+    width: 300px;
+    margin: 10px;
+    background-color: rgb(121, 63, 126);
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+}
+
+h4 {
+    color: white;
+    align-self: center;
+    padding: 20px 0px 50px;
+}
+
+button {
+    max-width: 150px;
+    align-self: center;
+    margin-bottom: 20px;
+}
+</style>
---
+++ vue-project/src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup>
+import HeaderInput from './HeaderInput.vue';
+</script>
+
+<template>
+    <header>
+        <h3 class="display-6"><a href="" target="_blank"><i class="bi bi-person-fill-exclamation"></i>Sobre</a></h3>
+        <h3 class="display-6"><a href="" target="_blank"><i class="bi bi-house"></i>Home</a></h3>
+        <h3 class="display-6"><a href="" target="_blank"><i class="bi bi-whatsapp"></i>Contato</a></h3>
+        <HeaderInput></HeaderInput>
+    </header>
+</template>
+
+<style scoped>
+header {
+    background-color: rgb(97, 12, 104);
+    padding: 30px;
+    display: flex;
+    justify-content: space-around;
+}
+
+a {
+    color: white;
+    text-decoration: none;
+}
+
+header .bi {
+    margin-right: 10px;
+}
+</style>

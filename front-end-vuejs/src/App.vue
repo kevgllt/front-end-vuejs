07140569bb5ff0b3d4f975b239a3866391062c9a
Kev 478 <60349527+kevgllt@users.noreply.github.com>
2024-08-09 13:57:40 +0000

    Create components again

---
+++ front-end-vuejs/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HeaderBar from './components/HeaderBar.vue';
+import CardsContent from './components/CardsContent.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <HeaderBar></HeaderBar>
+  <CardsContent></CardsContent>
+  <Footer></Footer>
+</template>
+
+<style scoped>
+  * {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+</style>
---
+++ front-end-vuejs/src/components/HeaderBar.vue
@@ -0,0 +1,31 @@
+<script setup>
+import LoginHeader from './LoginHeader.vue';
+</script>
+
+<template>
+    <header>
+        <h3 class="display-6"><a href="" target="_blank"><i class="bi bi-person-fill-exclamation"></i>Sobre</a></h3>
+        <h3 class="display-6"><a href="" target="_blank"><i class="bi bi-house"></i>Home</a></h3>
+        <h3 class="display-6"><a href="" target="_blank"><i class="bi bi-whatsapp"></i>Contato</a></h3>
+        <LoginHeader></LoginHeader>
+    </header>
+    
+</template>
+
+<style scoped>
+header {
+    background-color: rgb(97, 12, 104);
+    padding: 30px;
+    display: flex;
+    justify-content: space-around;
+}
+
+a {
+    color: white;
+    text-decoration: none;
+}
+
+header .bi { 
+  margin-right: 10px;
+}
+</style>
